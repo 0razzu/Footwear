f4b8b7f8b835c5727b1bf84f53ebb238a861d842
0razzu <0razzu@protonmail.com>
2020-12-06 00:44:39 +0600

    FootwearBuilder

---
+++ src/main/java/footwear/FootwearBuilder.java
@@ -0,0 +1,27 @@
+package footwear;
+
+
+import footwear.model.Footwear;
+import footwear.model.footwear_element.*;
+
+
+public interface FootwearBuilder {
+    void setCollar(Collar collar);
+    void setUpper(Upper upper);
+    void setTongue(Tongue tongue);
+    void setHardware(Hardware hardware);
+    void setLaces(Laces laces);
+    void setMidsole(Midsole midsole);
+    void setOutsole(Outsole outsole);
+    
+    FootwearBuilder withCollar(Collar collar);
+    FootwearBuilder withUpper(Upper upper);
+    FootwearBuilder withTongue(Tongue tongue);
+    FootwearBuilder withHardware(Hardware hardware);
+    FootwearBuilder withLaces(Laces laces);
+    FootwearBuilder withMidsole(Midsole midsole);
+    FootwearBuilder withOutsole(Outsole outsole);
+    
+    Footwear build();
+    void reset();
+}
