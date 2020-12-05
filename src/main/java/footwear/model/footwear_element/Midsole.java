7ab384e7440d4bb450a6e0ac15d8126820aa5e6f
0razzu <0razzu@protonmail.com>
2020-12-06 00:30:17 +0600

    Midsole

---
+++ src/main/java/footwear/model/footwear_element/Midsole.java
@@ -0,0 +1,13 @@
+package footwear.model.footwear_element;
+
+
+import footwear.model.footwear_element.color.MidsoleColor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class Midsole {
+    private MidsoleColor color;
+}
